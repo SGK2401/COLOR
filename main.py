1d770daec6ef02129f6a9f88153b169f1e9f848f
SGK2401 <sgk2401@qq.com>
2023-02-23 18:00:09 +0800

    Standard picture and test

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ from cv2 import fastNlMeansDenoisingColored
 
 @TIME
 def test():
-    print(IMAGE("./Picture/200.png").PROCESS("knc", True, None, None))
+    print(IMAGE("./Picture/RGB1.png").PROCESS("knc", True, None, None))
+    print(IMAGE("./Picture/RGB2.png").PROCESS("knc", True, None, None))
+    print(IMAGE("./Picture/RGB3.png").PROCESS("knc", True, None, None))
+    print("================")
+    print(IMAGE("./Picture/RGB1.png").PROCESS("log", True, None, None))
+    print(IMAGE("./Picture/RGB2.png").PROCESS("log", True, None, None))
+    print(IMAGE("./Picture/RGB3.png").PROCESS("log", True, None, None))
